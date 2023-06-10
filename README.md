c089412cc24aaccb90a8388396d25a35ab001b49
tcunningham203 <tcunningham203@gmail.com>
2023-06-10 22:36:18 +0900

    got all routes working, seed works, and insomnia is working

--- README.md
+++ README.md
@@ -27,11 +27,17 @@ As per the instructions, there is no link to the deployed application as with th
 ## Development
 In this section, I'll briefly discuss some of the challenges and successes with the project, as well as goals for the future.
 
+At first, it felt like things weren't working because I was doing the steps out of order. I got the sequelize set up, and then I did the schema.sql. But when I tried running the seeds, I just got a bunch of null values. I couldn't figure out what the problem was. Then I started actually reading the instructions and realized the steps were clearly laid out- I had to finish the models and routes before seeding. Once I realized that, the assignment was easy because the instructions on bootcampspot went step by step on what to do. I simply followed the instructions, referred back to previous activities in this module, and was able to finish it with only a few problems.
 
+Working with Insomnia took a lot of time. It was my first time really deep diving with it outside of class, so I had to look up a lot about how to use it. Eventually I was able to get it to look the way I wanted it to. It ended up being more approachable than I was expecting.
+
+This project will be a great reference for me in future projects working with MySQL and routing; I learned a lot. 
 
 ## Credits
 
 The code featured in the second commit "Starter Code" was taken from the following repo per the assignment instructions: https://github.com/coding-boot-camp/fantastic-umbrella
+
+From there, as mentioned in the development section, it was just a matter of following the steps laid out in the instructions. For example, the models step had the exact requirements for each model .js file, and I just plugged in the nessesary requirements and formatted it based on previous activities.
 
 I used various learning resources like Stack Overflow and W3 schools to help with random questions I had. 
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,76 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [Product],
+    });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!category[0]) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.json({ message: 'Category updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.json({ message: 'Category deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
